61500bfb140ee8c579f40244e135159fc24f31cb
EdineiMZ <113246537+EdineiMZ@users.noreply.github.com>
2024-06-07 23:15:43 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,16 +5,7 @@ Uma Calculadora Simples
 int main() {
     int opcao;
     float num1, num2, resultado;
-    std::cout << "Digite o primeiro numero: ";
-    std::cin >> num1;
-    std::cout << "Digite o segundo numero: ";
-    std::cin >> num2;
-    std::cout << "Escolha a operacao: \n";
-    std::cout << "1 - Soma\n";
-    std::cout << "2 - Subtracao\n";
-    std::cout << "3 - Multiplicacao\n";
-    std::cout << "4 - Divisao\n";
-    std::cin >> opcao;
+    
     switch (opcao) {
         case 1:
             resultado = num1 + num2;
